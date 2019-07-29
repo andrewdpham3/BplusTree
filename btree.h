d4ef52f63bc5d050f3d3b018d985c3431e746c08
Andrew Pham <andrewdpham3@gmail.com>
2019-07-29 03:01:10 -0400

    Update btree.h

--- btree.h
+++ btree.h
@@ -1,64 +1,282 @@
-/*
- * 
- * You will need to write your B+Tree almost entirely from scratch. 
- * 
- * B+Trees are dynamically balanced tree structures that provides efficient support for insertion, deletion, equality, and range searches. 
- * The internal nodes of the tree direct the search and the leaf nodes hold the base data..
- * 
- * For a basic rundown on B+Trees, we will refer to parts of Chapter 10 of the textbook Ramikrishnan-Gehrke 
- * (all chapters and page numbers in this assignment prompt refer to the 3rd edition of the textbook).
- *
- * Read Chapter 10 which is on Tree Indexing in general. In particular, focus on Chapter 10.3 on B+Tree.
- */
+//B+tree header
+//andrew pham anp6338@g.harvard.edu
 
 #ifndef BTREE_H
 #define BTREE_H
 
 #include "data_types.h"
 #include "query.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+//NODE STRUCT
+typedef struct node node;
+struct node{
+	int a,b;
+	int vala,valb;
+	node *p1, *p2, *p3;
+	bool leaf;
+};
+node* newnode(){
+    node * n=(node*)malloc(sizeof(node));
+    return n;
+}
 
-/* 
-Designing your C Structs for B+Tree nodes (Chapter 10.3.1)
-How will you represent a B+Tree as a C Struct (or series of C structs that work together)? There are many valid ways to do this part of your design, and we leave it open to you to try and tune this as you progress through the project.
-How will you account for a B+Tree node being an internal node or a leaf node? Will you have a single node type that can conditionally be either of the two types, or will you have two distinct struct types?
-How many children does each internal node have? This is called the fanout of the B+Tree.
-What is the maximum size for a leaf node? How about for an internal node?
-What is the minimum threshold of content for a node, before it has to be part of a rebalancing?
-*/
+//first and last follows pointers till end
+node* first(node* n){
+	if(n->p1!=0)
+		first(n->p1);
+	else
+		return n;
+	return n;
+}
+node* last(node* n){
+	if(n->p3!=0)
+		last(n->p3);
+	else
+		return n;
+	return n;
+}
 
-// TODO: here you will need to define a B+Tree node(s) struct(s)
+//addchildren adds a full set of children to a node
+void addchildren(node* n){
+	node* n1=newnode();
+	node* n2=newnode();
+	node* n3=newnode();
+	//initialize all values to zero
+	n1->a=0;
+	n1->b=0;
+	n2->a=0;
+	n2->b=0;
+	n3->a=0;
+	n3->b=0;
+	//point parent to children
+	n->p1=n1;
+	n->p2=n2;
+	n->p3=n3;
+	//in order children points to eachother
+	n1->p1=NULL;
+	n1->p3=n2;
+	n2->p1=n1;
+	n2->p3=n3;
+	n3->p1=n2;
+	n3->p3=NULL;
+	//set leaf
+	n->leaf=false;
+	n1->leaf=true;
+	n2->leaf=true;
+	n3->leaf=true;
+}
 
+//add level adds one depth to the tree
+void addlevel(node* n){
+	node* next=n->p3;
+	addchildren(n);
+	//printf("Children created for %p\n", n);
+	if(next!=0){
+		addlevel(next);
+		//link in order traversals
+		n->p3->p3=next->p1;
+		next->p1->p1=n->p3;
+	}
+}
 
-/* The following are methods that can be invoked on B+Tree node(s).
- * Hint: You may want to review different design patterns for passing structs into C functions.
- */
+//count data nodes
+int countdnodes(node* n){
+	int count=1;
+	if(n->p3!=0)
+		count+=countdnodes(n->p3);
+	return count;
+}
 
-/* FIND (Chapter 10.4)
-This is an equality search for an entry whose key matches the target key exactly.
-How many nodes need to be accessed during an equality search for a key, within the B+Tree? 
-*/
+//buildtree connects pointers of tree
+void buildtree(node* n){
+	if(!n->leaf){
+		buildtree(n->p1);
+		buildtree(n->p2);
+		buildtree(n->p3);
+		n->a = n->p1->b+1;
+		n->b = n->p3->a-1;
+	}
+}
 
-// TODO: here you will need to define FIND/SEARCH related method(s) of finding key-values in your B+Tree.
+//FIND
+int find(node* n, int a){
+	if(n->leaf && n->a==a)
+		return n->vala;
+	if(n->leaf && n->b==a)
+		return n->valb;
+	if(n->leaf)
+		return 0;
+	if(!n->leaf){
+		if(a < n->a)
+			find(n->p1,a);
+		if(a>n->a && a<n->b)
+			find(n->p2,a);
+		if(a>n->b)
+			find(n->p3,a);
+	}
+	return 0;
+}
 
+//shift moves data during an insert
+void shift(node* n, int a, int v){
+	int temp=a;
+	if(a <= n->a){
+		temp=n->b;
+		n->b=n->a;
+		n->a=a;
+		temp=n->valb;
+		n->valb=n->vala;
+		n->vala=v;
+	}
+	if(a > n->a && a < n->b){
+		temp=n->b;
+		n->b=a;
+		temp=n->valb;
+		n->valb=n->vala;
+	}
+	if(n->p3 != 0){
+		shift(n->p3,temp,v);
+	}
+	if(n->p3 == 0){
+		node* nn= newnode();
+		nn->a=temp;
+		nn->vala=v;
+		nn->b=0;
+		nn->p3=0;
+		nn->p1=n;
+		n->p3=nn;
+	}
+}
 
-/* INSERT (Chapter 10.5)
-How does inserting an entry into the tree differ from finding an entry in the tree?
-When you insert a key-value pair into the tree, what happens if there is no space in the leaf node? What is the overflow handling algorithm?
-For Splitting B+Tree Nodes (Chapter 10.8.3)
-*/
+//dynamic array struct
+typedef struct {
+  int *array;
+  size_t used;
+  size_t size;
+} Array;
+void initArray(Array *a, size_t initialSize) {
+	a->array = (int *)malloc(initialSize * sizeof(int));
+	a->used = 0;
+	a->size = initialSize;
+}
+void insertArray(Array *a, int element) {
+  if (a->used == a->size) {
+    a->size *= 2;
+    a->array = (int *)realloc(a->array, a->size * sizeof(int));
+  }
+  a->array[a->used++] = element;
+}
 
-// TODO: here you will need to define INSERT related method(s) of adding key-values in your B+Tree.
+//tree initializer
+node* newtree(Array * data ,int size){
+	printf("Initializing tree...\n");
+	node * root=newnode();
+	root->a=0;
+	root->b=0;
+	root->p1=0;
+	root->p3=0;
+	node* f=root;
+	int dataspots=2;
+	while(dataspots<size){
+		addlevel(f);
+		printf("Add a level\n");
+		f=first(f->p1);
+		dataspots=countdnodes(f)*2;
+		printf("We now have %i dataspots\n", dataspots);
+	}
+	
+	//assign the data to the leafs
+	f=first(root);
+	for(int i=0;i<size;){
+		f->a=data->array[i];
+		i++;
+		f->b=data->array[i];
+		i++;
+		f=f->p3;
+	}
+	printf("Data assigned to leafs\n");
+	//now give values to the tree...
+	buildtree(root);
+	return root;
+}
+node* emptytree(){
+	printf("Initializing tree...\n");
+	node * root=newnode();
+	root->a=0;
+	root->b=0;
+	root->p1=0;
+	root->p3=0;
+	addlevel(root);
+	printf("Children added!\n");
+	
+	//assign the data to the leafs
+	node* f=root->p1;
+	for(int i=0;i<6;){
+		f->a=0;
+		i++;
+		f->b=0;
+		i++;
+		f=f->p3;
+	}
+	printf("Data assigned to leafs\n");
+	//now give values to the tree...
+	buildtree(root);
+	printf("Tree Built!\n");
+	return root;
+}
 
+//INSERT
+node* insert(node* n, int a, int v){
+	printf("inserting %i\n", a);
+	if(n->leaf && n->a==0){
+		n->a=a;
+		n->vala=v;
+	}
+	if(n->leaf && n->b==0){
+		n->b=a;
+		n->valb=v;
+	}
+	if(n->leaf && n->b!=0)
+		shift(n, a, v);
+	if(!n->leaf){
+		//printf("not leaf, digging\n");
+		if(a < n->a)
+			insert(n->p1,a,v);
+		if(a >= n->a && a <= n->b)
+			insert(n->p2,a,v);
+		if(a>n->b)
+			insert(n->p3,a,v);
+	}
+	node* current=first(n);
+	Array b;
+	initArray(&b, 5);
+	for (int i=0;i<10;i++){
+		insertArray(&b, current->a);
+		insertArray(&b, current->b);
+		current=current->p3;
+	}
+	return newtree(&b, b.used);
+}
 
-/* BULK LOAD (Chapter 10.8.2)
-Bulk Load is a special operation to build a B+Tree from scratch, from the bottom up, when beginning with an already known dataset.
-Why might you use Bulk Load instead of a series of inserts for populating a B+Tree? Compare the cost of a Bulk Load of N data entries versus that of an insertion of N data entries? What are the tradeoffs?
-*/
-
-// TODO: here you will need to define BULK LOAD related method(s) of initially adding all at once some key-values to your B+Tree.
-// BULK LOAD only can happen at the start of a workload
-
+//UPDATE value
+void update(node* n, int a, int v){
+	if(n->leaf && n->a==a)
+		n->vala=v;
+	if(n->leaf && n->b==a)
+		n->valb=v;
+	if(!n->leaf){
+		//printf("not leaf, digging\n");
+		if(a < n->a)
+			update(n->p1,a,v);
+		if(a >= n->a && a <= n->b)
+			update(n->p2,a,v);
+		if(a>n->b)
+			update(n->p3,a,v);
+	}
+}
 
 /*RANGE (GRADUATE CREDIT)
 Scans are range searches for entries whose keys fall between a low key and high key.
@@ -69,7 +287,6 @@ Can you describe a generic cost expression for Scan, measured in number of rando
 */
 
 // TODO GRADUATE: here you will need to define RANGE for finding qualifying keys and values that fall in a key range.
-
 
 
 #endif
